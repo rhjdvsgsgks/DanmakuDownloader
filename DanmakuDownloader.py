7b99a57454ba00a73b44d3a764c91057a09d6c5d
rhjdvsgsgks <26178113+rhjdvsgsgks@users.noreply.github.com>
2019-10-07 10:59:31 -0500

    只有一部番可选时就不再问了

--- DanmakuDownloader.py
+++ DanmakuDownloader.py
@@ -8,11 +8,15 @@ import re
 import os
 import sys
 
-if len(sys.argv) is 1:
+def search():
+    global anime
     anime = input('番名?\n')
     if not anime:
         print('不说算了')
         sys.exit()
+
+if len(sys.argv) is 1:
+    search()
 else:
     for k in range(1,len(sys.argv)):
         if k is 1:
@@ -23,30 +27,46 @@ else:
 getepisode =  "https://api.acplay.net/api/v2/search/episodes?anime="
 downloadpath = "/sdcard/danmakudownload/"
 
-response = requests.get( getepisode + anime ).text
+def get():
+    global response,dict
+    response = requests.get( getepisode + anime ).text
+    dict = json.loads(response)
 
-dict = json.loads(response)
+get()
+
 if not dict['animes']:
     print('404')
-    sys.exit()
+    search()
+    get()
 
 animetitle = 'nill'
 episodes = 'null'
 def animelistselector():
-    for l in range(len(dict['animes'])):
-        print(str(l)+'.'+dict['animes'][l]['animeTitle'])
-    animelistnumber = input('哪个? ')
-    if animelistnumber.isdigit() and int(animelistnumber) in range(len(dict['animes'])):
-        global animetitle,episodes
-        animetitle = dict['animes'][int(animelistnumber)]['animeTitle']
-        episodes = dict['animes'][int(animelistnumber)]['episodes']
+    if not len(dict['animes']) is 1:
+        for l in range(len(dict['animes'])):
+            print(str(l)+'.'+dict['animes'][l]['animeTitle'])
+        animelistnumber = input('哪个? ')
+        if animelistnumber.isdigit() and int(animelistnumber) in range(len(dict['animes'])):
+            global animetitle,episodes
+            animetitle = dict['animes'][int(animelistnumber)]['animeTitle']
+            episodes = dict['animes'][int(animelistnumber)]['episodes']
+            if input('是 '+animetitle+' ? [y/N] ') is 'y':
+                pass
+            else:
+                animelistselector()
+        else:
+            print('不说算了')
+            sys.exit()
+    else:
+        animetitle = dict['animes'][0]['animeTitle']
+        episodes = dict['animes'][0]['episodes']
         if input('是 '+animetitle+' ? [y/N] ') is 'y':
             pass
         else:
+            print('404')
+            search()
+            get()
             animelistselector()
-    else:
-        print('不说算了')
-        sys.exit()
 
 animelistselector()
 
@@ -72,11 +92,11 @@ def downloaddanmaku(epid,eptitle):
         xml = '<?xml version="1.0" encoding="utf-8"?>'+ET.tostring(root,'utf-8').decode('utf-8')
         open(downloadpath+animetitle.replace('/','\\')+'/'+eptitle.replace('/','\\')+'.xml','w').write(xml)
         if os.path.exists('danmaku2ass.py'):
-            os.system('python danmaku2ass.py -s 3840x2160 -fs 100 -dm 20 -ds 20 -o '+'\"'+downloadpath+animetitle.replace('/','\\')+'/'+eptitle.replace('/','\\')+'.ass'+'\" \"'+downloadpath+animetitle.replace('/','\\')+'/'+eptitle.replace('/','\\')+'.xml'+'\"')
+            os.system('python danmaku2ass.py -s 3840x2160 -fs 100 -dm 30 -ds 30 -o '+'\"'+downloadpath+animetitle.replace('/','\\')+'/'+eptitle.replace('/','\\')+'.ass'+'\" \"'+downloadpath+animetitle.replace('/','\\')+'/'+eptitle.replace('/','\\')+'.xml'+'\"')
     else:
         print('跳过 '+eptitle)
 
 for i in range(episode_count):
     downloaddanmaku(episodes[i]['episodeId'],episodes[i]['episodeTitle'])
-    
+
 print(animetitle+' 下好了')
