8e8e7abe1a89d8e99c1c430d67d4e571e35e3b27
rhjdvsgsgks <26178113+rhjdvsgsgks@users.noreply.github.com>
2020-01-17 06:56:30 -0600

    Update DanmakuDownloader.py
    重写了流程部分

--- DanmakuDownloader.py
+++ DanmakuDownloader.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import requests
 import json
 try:
@@ -13,26 +15,31 @@ from copy import deepcopy
 import threading
 from opencc import OpenCC
 
-optlist, args = gnu_getopt(sys.argv[1:],'i:r')
 
-for m in range(len(optlist)):
-    if optlist[m][0] == '-i':
-        insertdanmakupath = optlist[m][1]
-        if 'insertdanmakupath' in dir() and os.path.exists(insertdanmakupath) and os.path.isdir(insertdanmakupath):
-            insertdanmaku = True
+def detectargs():
+    global args,insertdanmaku,insertdanmakupath,downloadfrombilibili,restoreinsertdanmaku,bilipath
+    optlist, args = gnu_getopt(sys.argv[1:],'i:rb:')
 
-for m in range(len(optlist)):
-    if optlist[m][0] == '-r' and insertdanmaku:
-        for i in os.listdir(insertdanmakupath):
-            if os.path.exists(insertdanmakupath+'/'+i+'/danmaku.xml.bak') and os.path.isfile(insertdanmakupath+'/'+i+'/danmaku.xml.bak'):
-                restoreinsertdanmaku = True
-            else:
-                print('没找到 .bak')
-                sys.exit()
+    for m in range(len(optlist)):
+        if optlist[m][0] == '-i':
+            insertdanmakupath = optlist[m][1]
+            if 'insertdanmakupath' in globals() and os.path.exists(insertdanmakupath) and os.path.isdir(insertdanmakupath):
+                insertdanmaku = True
+        if optlist[m][0] == '-b':
+            downloadfrombilibili = True
+            bilipath = optlist[m][1]
 
-if 'restoreinsertdanmaku' in dir() and restoreinsertdanmaku:
+    for m in range(len(optlist)):
+        if optlist[m][0] == '-r' and insertdanmaku:
+            restoreinsertdanmaku = True
+
+
+def restore():
+    bakexistindir = [os.path.exists(insertdanmakupath+'/'+i+'/danmaku.xml.bak') and os.path.isfile(insertdanmakupath+'/'+i+'/danmaku.xml.bak') for i in os.listdir(insertdanmakupath)]
+    if True not in bakexistindir:
+        print('没找到 .bak')
+        return
     [shutil.move(insertdanmakupath+'/'+i+'/danmaku.xml.bak', insertdanmakupath+'/'+i+'/danmaku.xml') for i in os.listdir(insertdanmakupath) if os.path.exists(insertdanmakupath+'/'+i+'/danmaku.xml.bak') and os.path.isfile(insertdanmakupath+'/'+i+'/danmaku.xml.bak')]
-    sys.exit()
 
 
 def search():
@@ -40,35 +47,33 @@ def search():
     anime = input('番名?\n')
     if not anime:
         print('不说算了')
-        sys.exit()
+        exit()
 
 
-if len(args) == 0:
-    if 'insertdanmaku' in dir() and insertdanmaku is True:
-        with open(insertdanmakupath+'/'+os.listdir(insertdanmakupath)[0]+'/entry.json','r') as entryjson:
-            entryjsondict = json.load(entryjson)
-            anime = OpenCC('t2s').convert(entryjsondict['title'])
-    else:
-        search()
-else:
-    for k in range(len(args)):
-        if k == 0:
-            anime = args[k]
+def getanime():
+    if len(args) == 0:
+        if 'insertdanmaku' in globals() and insertdanmaku is True:
+            with open(insertdanmakupath+'/'+os.listdir(insertdanmakupath)[0]+'/entry.json','r') as entryjson:
+                entryjsondict = json.load(entryjson)
+                anime = OpenCC('t2s').convert(entryjsondict['title'])
         else:
-            anime = anime+' '+args[k]
+            search()
+    else:
+        for k in range(len(args)):
+            if k == 0:
+                anime = args[k]
+            else:
+                anime = anime+' '+args[k]
 
-getepisode = "https://api.acplay.net/api/v2/search/episodes?anime="
+
 downloadpath = "/sdcard/danmaku/"
 subtitlepath = "/storage/emulated/0/Subtitles/"
 
 
 def get():
     global response,dict
-    response = requests.get( getepisode + anime ).text
+    response = requests.get('https://api.acplay.net/api/v2/search/episodes?anime='+anime).text
     dict = json.loads(response)
-
-
-get()
 
 
 def notempty():
@@ -77,12 +82,6 @@ def notempty():
         search()
         get()
         notempty()
-
-
-notempty()
-
-animetitle = 'nill'
-episodes = 'null'
 
 
 def animelistselector():
@@ -101,7 +100,7 @@ def animelistselector():
                 animelistselector()
         else:
             print('不说算了')
-            sys.exit()
+            exit()
     else:
         animetitle = dict['animes'][0]['animeTitle']
         episodes = dict['animes'][0]['episodes']
@@ -113,11 +112,22 @@ def animelistselector():
             search()
             get()
             animelistselector()
+    global episode_count
+    episode_count = len(episodes)
 
 
-animelistselector()
-
-episode_count = len(episodes)
+def reslovbili():
+    if bilipath[:2] == 'md':
+        #解析md
+        print('还没写')
+    elif bilipath[:2] == 'av':
+        #解析av
+        global plist,animetitle
+        avinfo = json.loads(requests.get('https://api.bilibili.com/x/web-interface/view?aid='+bilipath[2:]).text)['data']
+        plist = avinfo['pages']
+        animetitle = avinfo['title']
+    else:
+        print('仅支持av或md')
 
 
 def downloaddanmaku(epid,eptitle,numberinlist='',ptitle=''):
@@ -182,72 +192,111 @@ def downloaddanmaku(epid,eptitle,numberinlist='',ptitle=''):
         print('跳过 '+eptitle)
 
 
-if 'insertdanmaku' in dir() and insertdanmaku is True:
-    # 插入弹幕
-    entrydirlist = os.listdir(insertdanmakupath)
-    epcount = len(entrydirlist)
-    indexep = {}
-    indextitle = {}
-    for n in range(len(entrydirlist)):
-        with open(insertdanmakupath+'/'+entrydirlist[n]+'/entry.json','r') as entryjson:
-            entryjsondict = json.load(entryjson)
-        if 'avid' in entryjsondict :
-            # 视频
-            indexep[int(entryjsondict['page_data']['page'])],indextitle[int(entryjsondict['page_data']['page'])] = int(entrydirlist[n]),entryjsondict['page_data']['part']
+def bilidownloaddanmaku(cid,part):
+    print('在下 '+part)
+    response = requests.get('https://comment.bilibili.com/'+str(cid)+'.xml')
+    response.encoding = 'utf-8'
+    xml = response.text
+    if not os.path.exists(downloadpath+animetitle.replace('/','\\')):
+        os.makedirs(downloadpath+animetitle.replace('/','\\'))
+    open(downloadpath+animetitle.replace('/','\\')+'/'+part.replace('/','\\')+'.xml','w').write(xml)
+    if os.path.exists('danmaku2ass.py') and os.path.isfile('danmaku2ass.py'):
+        if not os.path.exists(subtitlepath+animetitle.replace('/','\\')):
+            os.makedirs(subtitlepath+animetitle.replace('/','\\'))
+        os.system('python danmaku2ass.py -s 3840x2160 -fs 85 -dm 20 -ds 20 -p 103 -o '+'\"'+subtitlepath+animetitle.replace('/','\\')+'/'+part.replace('/','\\')+'.ass'+'\" \"'+downloadpath+animetitle.replace('/','\\')+'/'+part.replace('/','\\')+'.xml'+'\"')
+
+
+def createtasklist():
+    global threads
+    if 'downloadfrombilibili' in globals() and downloadfrombilibili is True:
+        if 'insertdanmaku' in globals() and insertdanmaku is True:
+            print('从bilibili插入')
         else:
-            # 番剧
-            indexep[int(entryjsondict['ep']['index'])],indextitle[int(entryjsondict['ep']['index'])] = int(entryjsondict['ep']['episode_id']),entryjsondict['ep']['index_title']
-    existep = list(indextitle.keys())
-    hasextraep = False
-    for p in range(episode_count):
-        if episodes[p]['episodeTitle'][0] == 'S':
-            # 判断有附加p
-            hasextraep = True
-            break
-    if hasextraep is True:
-        # 有附加p,根据每p名称重新排序
-        sortedep = ['']*episode_count
-        episodesnonumber = deepcopy(episodes)
-        for o in range(episode_count):
-            splitedep = re.split(' ',episodesnonumber[o]['episodeTitle'])
-            if len(splitedep) > 1 and splitedep[0][1].isdigit():
-                del splitedep[0]
-                episodesnonumber[o]['episodeTitle'] = ' '.join(splitedep)
-            for p in existep:
-                if episodesnonumber[o]['episodeTitle'].lower() == indextitle[p].lower():
-                    sortedep[p-1] = episodes[o]
-                    episodes[o] = ''
-        if len([x for x in sortedep if x != '']) != epcount:
-            #判断非空位
-            print('有多出来的,使用id匹配')
-            unusedepisodes = [x for x in indextitle if sortedep[x-1] == '']
-            for i in unusedepisodes:
-                tempinput = input(str(i)+' '+indextitle[i]+' 是 '+episodes[i-1]['episodeTitle']+' ? [Y/n] ')
-                if tempinput == 'y' or tempinput == '':
-                    sortedep[i-1] = episodes[i-1]
-                    episodes[i-1] = ''
-        if len([sortedep[x-1] for x in indextitle.keys() if sortedep[x-1] == '']) > 0:
-            #判断在按本地ep排序的episodes中有几个空位
-            print('还是有多的，使用手动匹配')
-            for i in indextitle.keys():
-                if sortedep[i-1] == '':
-                    for k in [j for j in range(len(episodes)) if episodes[j] != '']:
-                        print(str(k)+' '+episodes[k]['episodeTitle'])
-                    selectepisode = int(input(str(i)+' '+indextitle[i]+'是? '))
-                    sortedep[i-1] = episodes[selectepisode]
-                    episodes[selectepisode] = ''
-        threads = [threading.Thread(target=downloaddanmaku,args=[sortedep[i-1]['episodeId'],sortedep[i-1]['episodeTitle'],indexep[i],indextitle[i]]) for i in indexep.keys()]
+            print('从bilibili下载')
+            threads = [threading.Thread(target=bilidownloaddanmaku,args=[i['cid'],i['part']]) for i in plist]
     else:
-        # 没附加p
-        threads = [threading.Thread(target=downloaddanmaku,args=[episodes[i-1]['episodeId'],episodes[i-1]['episodeTitle'],indexep[i],indextitle[i]]) for i in indexep.keys()]
+        if 'insertdanmaku' in globals() and insertdanmaku is True:
+            # 插入弹幕
+            entrydirlist = os.listdir(insertdanmakupath)
+            epcount = len(entrydirlist)
+            indexep = {}
+            indextitle = {}
+            for n in range(len(entrydirlist)):
+                with open(insertdanmakupath+'/'+entrydirlist[n]+'/entry.json','r') as entryjson:
+                    entryjsondict = json.load(entryjson)
+                if 'avid' in entryjsondict :
+                    # 视频
+                    indexep[int(entryjsondict['page_data']['page'])],indextitle[int(entryjsondict['page_data']['page'])] = int(entrydirlist[n]),entryjsondict['page_data']['part']
+                else:
+                    # 番剧
+                    indexep[int(entryjsondict['ep']['index'])],indextitle[int(entryjsondict['ep']['index'])] = int(entryjsondict['ep']['episode_id']),entryjsondict['ep']['index_title']
+            existep = list(indextitle.keys())
+            hasextraep = False
+            for p in range(episode_count):
+                if episodes[p]['episodeTitle'][0] == 'S':
+                    # 判断有附加p
+                    hasextraep = True
+                    break
+            if hasextraep is True:
+                # 有附加p,根据每p名称重新排序
+                sortedep = ['']*episode_count
+                episodesnonumber = deepcopy(episodes)
+                for o in range(episode_count):
+                    splitedep = re.split(' ',episodesnonumber[o]['episodeTitle'])
+                    if len(splitedep) > 1 and splitedep[0][1].isdigit():
+                        del splitedep[0]
+                        episodesnonumber[o]['episodeTitle'] = ' '.join(splitedep)
+                    for p in existep:
+                        if episodesnonumber[o]['episodeTitle'].lower() == indextitle[p].lower():
+                            sortedep[p-1] = episodes[o]
+                            episodes[o] = ''
+                if len([x for x in sortedep if x != '']) != epcount:
+                    #判断非空位
+                    print('有多出来的,使用id匹配')
+                    unusedepisodes = [x for x in indextitle if sortedep[x-1] == '']
+                    for i in unusedepisodes:
+                        tempinput = input(str(i)+' '+indextitle[i]+' 是 '+episodes[i-1]['episodeTitle']+' ? [Y/n] ')
+                        if tempinput == 'y' or tempinput == '':
+                            sortedep[i-1] = episodes[i-1]
+                            episodes[i-1] = ''
+                if len([sortedep[x-1] for x in indextitle.keys() if sortedep[x-1] == '']) > 0:
+                    #判断在按本地ep排序的episodes中有几个空位
+                    print('还是有多的，使用手动匹配')
+                    for i in indextitle.keys():
+                        if sortedep[i-1] == '':
+                            for k in [j for j in range(len(episodes)) if episodes[j] != '']:
+                                print(str(k)+' '+episodes[k]['episodeTitle'])
+                            selectepisode = int(input(str(i)+' '+indextitle[i]+'是? '))
+                            sortedep[i-1] = episodes[selectepisode]
+                            episodes[selectepisode] = ''
+                threads = [threading.Thread(target=downloaddanmaku,args=[sortedep[i-1]['episodeId'],sortedep[i-1]['episodeTitle'],indexep[i],indextitle[i]]) for i in indexep.keys()]
+            else:
+                # 没附加p
+                threads = [threading.Thread(target=downloaddanmaku,args=[episodes[i-1]['episodeId'],episodes[i-1]['episodeTitle'],indexep[i],indextitle[i]]) for i in indexep.keys()]
+        else:
+            # 正常下载
+            threads = [threading.Thread(target=downloaddanmaku,args=[episodes[i]['episodeId'],episodes[i]['episodeTitle']]) for i in range(episode_count)]
+
+
+detectargs()
+if 'restoreinsertdanmaku' in globals() and restoreinsertdanmaku:
+    restore()
 else:
-    # 正常下载
-    threads = [threading.Thread(target=downloaddanmaku,args=[episodes[i]['episodeId'],episodes[i]['episodeTitle']]) for i in range(episode_count)]
+    if 'downloadfrombilibili' in globals() and downloadfrombilibili is True:
+        #解析b站然后直接创建threads
+        reslovbili()
+        createtasklist()
+    else:
+        getanime()
+        get()
+        notempty()
+        animelistselector()
+        createtasklist()
 
-for i in threads:
-    i.start()
+    for i in threads:
+        i.start()
 
-for i in threads:
-    i.join()
+    for i in threads:
+        i.join()
 
-print(animetitle+' 下好了')
+    print(animetitle+' 下好了')
